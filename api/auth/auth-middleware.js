bdfeca806de211c204c0d7ba4eb49eb644525c3b
Rebecca-Stone <primstone16@gmail.com>
2022-04-26 21:58:46 -0500

    all tests passing, code cleaned up

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,23 +1,8 @@
-const { JWT_SECRET } = require("../secrets"); // use this secret!
+const { JWT_SECRET } = require("../secrets");
 const { findBy } = require("../users/users-model");
 const jwt = require("jsonwebtoken");
 
 const restricted = (req, res, next) => {
-  /*
-    If the user does not provide a token in the Authorization header:
-    status 401
-    {
-      "message": "Token required"
-    }
-
-    If the provided token does not verify:
-    status 401
-    {
-      "message": "Token invalid"
-    }
-
-    Put the decoded token in the req object, to make life easier for middlewares downstream!
-  */
   const token = req.headers.authorization;
   if (!token) {
     return next({ status: 401, message: "Token required" });
@@ -33,16 +18,6 @@ const restricted = (req, res, next) => {
 };
 
 const only = (role_name) => (req, res, next) => {
-  /*
-    If the user does not provide a token in the Authorization header with a role_name
-    inside its payload matching the role_name passed to this function as its argument:
-    status 403
-    {
-      "message": 
-    }
-
-    Pull the decoded token from the req object, to avoid verifying it again!
-  */
   if (role_name === req.decodedToken.role_name) {
     next();
   } else {
@@ -51,13 +26,6 @@ const only = (role_name) => (req, res, next) => {
 };
 
 const checkUsernameExists = async (req, res, next) => {
-  /*
-    If the username in req.body does NOT exist in the database
-    status 401
-    {
-      "message": ""
-    }
-  */
   try {
     const [user] = await findBy({ username: req.body.username });
     if (!user) {
@@ -72,24 +40,6 @@ const checkUsernameExists = async (req, res, next) => {
 };
 
 const validateRoleName = (req, res, next) => {
-  /*
-    If the role_name in the body is valid, set req.role_name to be the trimmed string and proceed.
-
-    If role_name is missing from req.body, or if after trimming it is just an empty string,
-    set req.role_name to be 'student' and allow the request to proceed.
-
-    If role_name is 'admin' after trimming the string:
-    status 422
-    {
-      "message": ""
-    }
-
-    If role_name is over 32 characters after trimming the string:
-    status 422
-    {
-      "message": ""
-    }
-  */
   if (!req.body.role_name || !req.body.role_name.trim()) {
     req.role_name = "student";
     next();
