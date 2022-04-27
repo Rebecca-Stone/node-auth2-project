c86adb3a28131f56917b884ef1454f8deb47d059
Rebecca-Stone <primstone16@gmail.com>
2022-04-26 21:16:20 -0500

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,4 +1,5 @@
 const { JWT_SECRET } = require("../secrets"); // use this secret!
+const { findBy } = require("../users/users-model");
 
 const restricted = (req, res, next) => {
   /*
@@ -33,15 +34,25 @@ const only = (role_name) => (req, res, next) => {
   next();
 };
 
-const checkUsernameExists = (req, res, next) => {
+const checkUsernameExists = async (req, res, next) => {
   /*
     If the username in req.body does NOT exist in the database
     status 401
     {
-      "message": "Invalid credentials"
+      "message": ""
     }
   */
-  next();
+  try {
+    const [user] = await findBy({ username: req.body.username });
+    if (!user) {
+      next({ status: 422, message: "Invalid credentials" });
+    } else {
+      req.user = user;
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
 };
 
 const validateRoleName = (req, res, next) => {
@@ -71,6 +82,7 @@ const validateRoleName = (req, res, next) => {
   } else if (req.body.role_name.trim().length > 32) {
     next({ status: 422, message: "Role name can not be longer than 32 chars" });
   } else {
+    req.role_name = req.body.role_name.trim();
     next();
   }
 };
