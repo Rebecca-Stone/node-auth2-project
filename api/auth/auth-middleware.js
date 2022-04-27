f033bee23aafba42bdabfe13700b5b2cd203278f
Rebecca-Stone <primstone16@gmail.com>
2022-04-26 19:44:28 -0500

    working in middleware and model

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -17,9 +17,9 @@ const restricted = (req, res, next) => {
     Put the decoded token in the req object, to make life easier for middlewares downstream!
   */
   next();
-}
+};
 
-const only = role_name => (req, res, next) => {
+const only = (role_name) => (req, res, next) => {
   /*
     If the user does not provide a token in the Authorization header with a role_name
     inside its payload matching the role_name passed to this function as its argument:
@@ -30,8 +30,8 @@ const only = role_name => (req, res, next) => {
 
     Pull the decoded token from the req object, to avoid verifying it again!
   */
-}
-
+  next();
+};
 
 const checkUsernameExists = (req, res, next) => {
   /*
@@ -41,8 +41,8 @@ const checkUsernameExists = (req, res, next) => {
       "message": "Invalid credentials"
     }
   */
-}
-
+  next();
+};
 
 const validateRoleName = (req, res, next) => {
   /*
@@ -54,20 +54,30 @@ const validateRoleName = (req, res, next) => {
     If role_name is 'admin' after trimming the string:
     status 422
     {
-      "message": "Role name can not be admin"
+      "message": ""
     }
 
     If role_name is over 32 characters after trimming the string:
     status 422
     {
-      "message": "Role name can not be longer than 32 chars"
+      "message": ""
     }
   */
-}
+  if (!req.body.role_name || !req.body.role_name.trim()) {
+    req.role_name = "student";
+    next();
+  } else if (req.body.role_name.trim() === "admin") {
+    next({ status: 422, message: "Role name can not be admin" });
+  } else if (req.body.role_name.trim().length > 32) {
+    next({ status: 422, message: "Role name can not be longer than 32 chars" });
+  } else {
+    next();
+  }
+};
 
 module.exports = {
   restricted,
   checkUsernameExists,
   validateRoleName,
   only,
-}
+};
